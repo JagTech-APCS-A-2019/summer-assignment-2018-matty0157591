9bed2f42b980faabb49215ee2b08885cb6d4b1e9
matty0157591 <39890761+matty0157591@users.noreply.github.com>
2018-08-08 13:56:19 -0400

    Delete FirstProg.java

--- Exercise 1/FirstProg.java
+++
@@ -1,8 +0,0 @@
-FirstProg.java 
-
-Public class FirstProg {
-Public static void main (String [] args) {
-	System.out.println( "I am determined t learn to code");
-	System.out.println ("Today's date is");
-}
-}
