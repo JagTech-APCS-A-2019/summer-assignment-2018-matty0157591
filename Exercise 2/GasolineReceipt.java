bb0657d92744ed3e8ee8fe3fbddc2e237fce7e35
matty0157591 <39890761+matty0157591@users.noreply.github.com>
2018-08-08 15:48:36 -0400

    Add files via upload

---
+++ Exercise 2/GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   GASOLINE PRICES      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2018-08-08  04:38PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:       9.86    |" );
+		System.out.println( "| Price/gallon: $ 2.865  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total:  $ 26.78   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
+}
+
