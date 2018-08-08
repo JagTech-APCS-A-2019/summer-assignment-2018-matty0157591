77c40d050b4a5cc4a87c3b072f6566f8e20cbe0e
matty0157591 <39890761+matty0157591@users.noreply.github.com>
2018-08-08 13:45:38 -0400

    Add files via upload

---
+++ Exercise 1/FirstProg.java
@@ -0,0 +1,8 @@
+FirstProg.java 
+
+Public class FirstProg {
+Public static void main (String [] args) {
+	System.out.println( "I am determined t learn to code");
+	System.out.println ("Today's date is");
+}
+}
